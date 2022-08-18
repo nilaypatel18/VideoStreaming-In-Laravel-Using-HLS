788d534a2910974755ec893e6c95a376b87b2118
Nilay Patel <48822794+nilaypatel18@users.noreply.github.com>
2022-08-18 12:30:12 +0530

    Add files via upload

---
+++ storage/framework/views/e5978775c55376174aaabe932dac5967c099b1a9.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Dash.js Rocks</title>
+        <style>
+            video {
+                width: 640px;
+                height: 360px;
+            }
+        </style>
+    </head>
+    <body>
+        <div>
+            <video data-dashjs-player autoplay src="videos/mainfest.mpd" controls>
+            </video>
+        </div>
+        <script src="hls/dash.all.min.js"></script>
+    </body>
+</html><?php /**PATH C:\xampp\htdocs\Laravel\videostream\resources\views/welcome.blade.php ENDPATH**/ ?>
